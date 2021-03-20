61d55b419af5a1d6cfdf67457653a65e9d3ec262
Sahasra Ranjan <sahasraranjan@gmail.com>
2021-03-20 22:46:07 +0530

    done

--- 6/q2/ThreebitUpDownCount.vhd
+++ 6/q2/ThreebitUpDownCount.vhd
@@ -12,8 +12,8 @@ end entity;
 
 architecture structure of ThreebitUpDownCount is
 
-Signal S,T,Sc:std_logic_vector(2 downto 0);
-Signal v0,v1,v2,v3,S0c,S1c:std_logic;
+Signal Q,T,Qc:std_logic_vector(2 downto 0);
+Signal v0,v1,v2,v3,v5,v6,v7,v8:std_logic;
 Signal upc:std_logic;
 
 -- declare components of basic_gates and flipflop
@@ -22,7 +22,27 @@ component and_gate
 			c:out std_logic);
 end component;
 
+component and3_gate
+	port(a,b,c : in std_logic;
+			d:out std_logic);
+end component;
+
+component and4_gate
+	port(a,b,c,d : in std_logic;
+			e:out std_logic);
+end component;
+
 component or_gate
+	port(a,b : in std_logic;
+			c:out std_logic);
+end component;
+
+component or5_gate
+	port(a,b,c,d,e : in std_logic;
+			f:out std_logic);
+end component;
+
+component xor_gate
 	port(a,b : in std_logic;
 			c:out std_logic);
 end component;
@@ -38,59 +58,71 @@ end component;
 
 begin
 
---From kmap T0 = 1
-T(0) <= '1';
+--From kmap T0 = Q0'
+notgate4 : not_gate
+port map(Q(0),Qc(0));
+
+T(0) <= Qc(0);
 
 
+--From Kmap T1 = up'.(Q0 xor Q1)' + up.(Q0 xor Q1)
+xorgate0 : xor_gate
+port map(Q(0),Q(1),v1);
 
---From Kmap T1 = up'.S0 + up.S0'
 notgate0 : not_gate
 port map(up,upc);
 
 notgate1 : not_gate
-port map(S(0),Sc(0));
+port map(v1,v2);
 
 andgate0 : and_gate
-port map(upc,S(0),v2);
+port map(v1,up,v3);
 
 andgate1 : and_gate
-port map(up,Sc(0),v3);
+port map(v2,upc,v4);
 
 orgate1 : or_gate
-port map(v2,v3,T(1));
+port map(v4,v3,T(1));
 
 
 
 -- From Kmap T2 = up'.S1.S0 + up.S1'.S0'
-andgate2 : and_gate
-port map(v2,S(1),v0);
-
 notgate2 : not_gate
-port map(S(1),Sc(1));
+port map(Q(1),Qc(1));
+notgate2 : not_gate
+port map(Q(2),Qc(2));
 
-andgate3 : and_gate
-port map(v3,Sc(1),v1);
+andgate2 : and3_gate
+port map(Q(2),Q(1),Qc(0),v5);
+andgate3 : and3_gate
+port map(up,Q(2),Qc(1),v6);
+andgate4 : and3_gate
+port map(upc,Q(0),Q(2),v7);
 
-orgate0 : or_gate
-port map(v0, v1, T(2));
+andgate5 : and4_gate
+port map(upc,Qc(0),Qc(1),Qc(2),v8);
+andgate6 : and4_gate
+port map(up,Q(0),Q(1),Qc(2),v9);
 
+orgate2 : or5_gate
+port map(v5,v6,v7,v8,v9,T(2));
 
 
 --Now we will use flipflops to get the values for next iterations, each of T0,T1,T2 have been fed into the flipflops
 ff1 : DFlipFlop
-port map(clk,rst,T(2),S(2));
+port map(clk,rst,T(2),Q(2));
 
 ff2 : DFlipFlop
-port map(clk,rst,T(1),S(1));
+port map(clk,rst,T(1),Q(1));
 
 ff3 : DFlipFlop
-port map(clk,rst,T(0),S(0));
+port map(clk,rst,T(0),Q(0));
 
 
 --The output is simply the bits in S as defined by our states
-count(2) <= S(2);
-count(1) <= S(1);
-count(0) <= S(0);
+count(2) <= Q(2);
+count(1) <= Q(1);
+count(0) <= Q(0);
 
 end architecture;
 
